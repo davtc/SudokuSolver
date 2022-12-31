893c701b2c0115ed5587ca132856c504872a04c7
davtc <47212365+davtc@users.noreply.github.com>
2023-01-01 00:38:15 +1100

    Added some request routes

--- app/routes.py
+++ app/routes.py
@@ -1,3 +1,40 @@
 from app import app
 from flask import request, jsonify
 
+from sudokusolver import validate_sudoku, init_possible_values, solve_sudoku, check_solved
+
+@app.route('/validate', methods = ['POST'])
+def validate():
+    data = request.json()
+    sudoku = data.get('sudoku')
+    print(sudoku)
+    response = jsonify({
+        'validate': validate_sudoku(sudoku)
+    })
+    return response
+
+@app.route('/solve', methods = ['POST'])
+def solve():
+    data = request.json()
+    sudoku = data.get('sudoku')
+    if validate_sudoku(sudoku):
+        values = init_possible_values(sudoku)
+        solution, solved = solve_sudoku(sudoku, values)
+        if solved and check_solved(sudoku):
+            response = jsonify({
+                'message': 'SUCCESS',
+                'solved': solved,
+                'solution': solution
+            })
+        else:             
+            response = jsonify({
+                'message': 'UNSUCCESSFUL',
+                'solved': solved,
+                'solution': sudoku
+            })
+    else:
+        response = jsonify({
+                'message': 'INVALID'
+            })
+
+    return response
