a0e519c20074be613691d6d731c31de428d679ba
davtc <47212365+davtc@users.noreply.github.com>
2023-01-04 16:57:50 +1100

    Added a tab component for loading sudokus

---
+++ frontend/src/components/ImageTab.js
@@ -0,0 +1,10 @@
+import './Tab.css'
+
+function ImageTab() {
+    return(
+        <div>
+            <p>Upload an image of a Sudoku puzzle:</p>
+            <button>Upload</button>
+        </div>
+    );
+} export default ImageTab
