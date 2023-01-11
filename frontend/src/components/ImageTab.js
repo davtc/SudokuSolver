f2b6942b8f51b63603d135e12e2d9dffa92ea4ad
davtc <47212365+davtc@users.noreply.github.com>
2023-01-12 01:29:17 +1100

    Added file upload function

--- frontend/src/components/ImageTab.js
+++ frontend/src/components/ImageTab.js
@@ -1,11 +1,28 @@
 import './Tab.css'
 import ButtonControl from './ButtonControl';
+import { useState } from 'react';
 
 function ImageTab() {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = e => {
+        setFile(e.target.files[0]);
+    };
+
+    const onFileSubmit = e => {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('imageFile', file)
+
+    }
+
     return(
         <div>
-            <p>Upload an image of a Sudoku puzzle:</p>
-            <ButtonControl name='Upload' />
+            <form onSubmit={onFileSubmit}>
+                <p>Upload an image of a Sudoku puzzle:</p>
+                <input type='file' onChange={handleFileChange}/>
+                <Button type='submit'>Submit</Button>
+            </form>
         </div>
     );
 } export default ImageTab
